dc99a83291133fee3d72321db12c983a8edb7e80
yldas <misael.valentin@upr.edu>
2019-01-20 03:15:44 -0400

    Added interactive bar chart for top Python repos.

--- python_repos.py
+++ python_repos.py
@@ -1,4 +1,6 @@
 import requests
+import pygal
+from pygal.style import LightColorizedStyle as LCS, LightenStyle as LS
 
 # Make an API call and store the response.
 url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
@@ -9,4 +11,42 @@ print("Status code:", r.status_code)
 response_dict = r.json()
 
 # Process results.
-print(response_dict.keys())
+print("Total repositories:", response_dict['total_count'])
+
+# Explore information about the repositories.
+repo_dicts = response_dict['items']
+print("Number of items:", len(repo_dicts))
+
+names, plot_dicts = [], []
+for repo_dict in repo_dicts:
+    names.append(repo_dict['name'])
+    if repo_dict['description']:
+        description = repo_dict['description']
+    else:
+        description = 'None'
+    plot_dict = {
+        'value': repo_dict['stargazers_count'],
+        'label': description,
+        'xlink': repo_dict['html_url'],
+        }
+    plot_dicts.append(plot_dict)
+
+# Make a visualization.
+my_style = LS('#333366', base_style=LCS)
+
+my_config = pygal.Config()
+my_config.x_label_rotation = 45
+my_config.show_legend = False
+my_config.title_font_size = 24
+my_config.label_font_size = 14
+my_config.major_label_font_size = 18
+my_config.truncate_label = 15
+my_config.show_y_guides = False
+my_config.width = 1000
+
+chart = pygal.Bar(my_config, style=my_style)
+chart.title = 'Most-Starred Python Projects on Github'
+chart.x_labels = names
+
+chart.add('', plot_dicts)
+chart.render_to_file('python_repos.svg')
